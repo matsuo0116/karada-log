09893ae599565e9d9325c8601d0a05aa9a330de7
matsuo0116 <matsuo0116@gmail.com>
2023-07-03 18:46:09 +0900

    ログイン画面のリンクを修正

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -35,11 +35,11 @@
                 {{ __('Log in') }}
             </x-primary-button>
             <a href="{{ route('register') }}" class="green_btn">新規登録</a>
-            @if (Route::has('password.request'))
+            {{--@if (Route::has('password.request'))
                 <a class="forgot_pass underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
                     {{ __('Forgot your password?') }}
                 </a>
-            @endif
+            @endif--}}
 
             
         </div>
